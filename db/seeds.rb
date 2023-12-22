642682ccad3bc45ea1e54721408ce83c5e47e883
SekoViper <damehlivingstone1@gmail.com>
2023-12-22 14:29:02 +0000

    Add seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+News.create([
+  { title: 'Sample News 1', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSf_C3OjH3BbicdZ1UP0jAncMv-HpNvU_B1fg6C8H_vcg&s' },
+  { title: 'Sample News 2', content: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', image: 'https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg' },
+])
+
+puts 'Seed data created successfully.'
