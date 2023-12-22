4657085fc75572e444f5f888bd3d23f12ac0177a
SekoViper <damehlivingstone1@gmail.com>
2023-12-22 14:25:06 +0000

    Updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ["http://localhost:3001", "http://localhost:5173", "http://localhost:3000, https://posterose.onrender.com"]
+    origins ["http://localhost:3001", "http://localhost:5173", "http://localhost:3000"]
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
+
