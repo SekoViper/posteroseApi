3228fe83df57dc3de3c778d4a684d2e9b2332b7f
SekoViper <damehlivingstone1@gmail.com>
2023-12-22 14:11:36 +0000

    Update render file.

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 # exit on error
+# set -o errexit
+
+# bundle install
+# ./bin/rails assets:precompile
+# ./bin/rails rake assets:clean
+
+
+#!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 bundle install
-./bin/rails assets:precompile
-./bin/rails rake assets:clean
+bundle exec rake db:migrate
+# bundle exec rails db:seed
